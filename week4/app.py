2235857d9bf3b64f9e2a8d9a852ff85d17022b78
Madoka Fujii <madokafuji@yahoo.com>
2024-12-04 09:24:48 -0500

    Update render.yaml and reflect file deletions

--- week4/app.py
+++ week4/app.py
@@ -67,4 +67,4 @@ def predict():
     return render_template('index.html', prediction_text='Loan Acceptance {}'.format(output))
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
